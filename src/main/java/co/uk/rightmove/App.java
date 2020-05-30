0abeb926d76ec535920af1de7a3793dc7280db84
tunde2018 <tunadng@yahoo.com>
2020-05-30 21:45:16 +0100

    adding initial test framework

---
+++ src/main/java/co/uk/rightmove/App.java
@@ -0,0 +1,13 @@
+package co.uk.rightmove;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
